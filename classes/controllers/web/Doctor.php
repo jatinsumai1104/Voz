13df23a4c9bf7a83b179ec6ed6587b728adb7c2c
nikhil-ghind <nikhilghind29@gmail.com>
2020-01-17 02:40:59 +0530

    Data For Records

--- classes/controllers/web/Doctor.php
+++ classes/controllers/web/Doctor.php
@@ -7,7 +7,7 @@ Class Doctor{
     }
 
     public function getRecentRecords($doctor_id){
-        $query = "";
+        $query = "SELECT * FROM prescription,cases,patient,users WHERE prescription.case_id = case.id AND prescription.doctor_id = $doctor_id AND patient.id = cases.patient_id AND patient.user_id = users.id";
         $res = $this->di->rawQuery($query);
         return $res;
     }
