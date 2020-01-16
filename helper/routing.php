c8cf8ea187aff521b42b71903dcadbca53c7b3b8
jatinsumai1104 <jatinsumai1104@gmail.com>
2020-01-17 02:55:48 +0530

    insertion of patient through doctor

--- helper/routing.php
+++ helper/routing.php
@@ -5,3 +5,10 @@
 if(isset($_POST["voice_text"])){
     var_dump($_POST);
 }
+
+if(isset($_POST["patient_register_through_doctor"])){
+    $dataURL = $_POST["image"];
+    $dataURL = str_replace('data:image/png;base64,', '', $dataURL);
+    $dataURL = str_replace(' ', '+', $dataURL);
+    $_POST['image'] = base64_decode($dataURL);
+}
--- views/sections/register_patient.php
+++ views/sections/register_patient.php
@@ -14,70 +14,117 @@ require_once('../includes/header-bp.php');
         <div class="col-12">
           <h2 class="contact-title">Patient Details</h2>
         </div>
-        <div class="col-lg-8">
-          <form class="form-contact contact_form" action="contact_process.php" method="post" id="contactForm"
+        <div class="col-lg-10">
+          <form class="form-contact contact_form" action="<?php echo BASEURL?>helper/routing.php" method="post" id="contactForm"
             novalidate="novalidate">
-            <div class="row">
-              <div class="col-12">
-                <div class="form-group">
-
-                  <textarea class="form-control w-100" name="message" id="message" cols="30" rows="9"
-                    onfocus="this.placeholder = ''" onblur="this.placeholder = 'Enter Message'"
-                    placeholder='Enter Message'></textarea>
+            <div class="form-contact contact_form">
+              <div class="row">
+                <div class="col-sm-6">
+                  <div class="form-group">
+                    <input class="form-control" name="name" id="name" type="text" onfocus="this.placeholder = ''"
+                      onblur="this.placeholder = 'Enter your name'" placeholder='Enter your name'>
+                  </div>
+                </div>
+                <div class="col-sm-6">
+                  <div class="form-group">
+                    <input class="form-control" name="email" id="email" type="email" onfocus="this.placeholder = ''"
+                      onblur="this.placeholder = 'Enter email address'" placeholder='Enter email address'>
+                  </div>
+                </div>
+                <div class="col-sm-6">
+                  <div class="form-group">
+                  <select class="form-control" id="Select" name="gender">
+                        <option value="Male" selected>Male</option>
+                        <option value="Female">Female</option>
+                    </select>
+                  </div>
+                </div>
+                <div class="col-sm-6">
+                  <div class="form-group">
+                    <input class="form-control" name="dob" id="dob" type="date" onfocus="this.placeholder = ''"
+                      onblur="this.placeholder = 'Enter Date Of Birth'" placeholder='Enter Date Of Birth'>
+                  </div>
+                </div>
+                <div class="col-sm-6">
+                  <div class="form-group">
+                  <input id="image" name="image" hidden>
+                  <canvas id="myCanvas" width="400" height="350" style="border: 10px;display: none"></canvas>
+                  <video onclick="snapshot(this);" id="video" width="100%" height="100%" controls autoplay></video>
+                  </div>
                 </div>
               </div>
-              <div class="col-sm-6">
-                <div class="form-group">
-                  <input class="form-control" name="name" id="name" type="text" onfocus="this.placeholder = ''"
-                    onblur="this.placeholder = 'Enter your name'" placeholder='Enter your name'>
-                </div>
+              <div class="form-group mt-3">
+                <button type="submit" class="button button-contactForm btn_1" name="patient_register_through_doctor">Register</button>
               </div>
-              <div class="col-sm-6">
-                <div class="form-group">
-                  <input class="form-control" name="email" id="email" type="email" onfocus="this.placeholder = ''"
-                    onblur="this.placeholder = 'Enter email address'" placeholder='Enter email address'>
-                </div>
-              </div>
-              <div class="col-12">
-                <div class="form-group">
-                  <input class="form-control" name="subject" id="subject" type="text" onfocus="this.placeholder = ''"
-                    onblur="this.placeholder = 'Enter Subject'" placeholder='Enter Subject'>
-                </div>
-              </div>
-            </div>
-            <div class="form-group mt-3">
-              <button type="submit" class="button button-contactForm btn_1">Send Message</button>
             </div>
           </form>
         </div>
-        <div class="col-lg-4">
-          <div class="media contact-info">
-            <span class="contact-info__icon"><i class="ti-home"></i></span>
-            <div class="media-body">
-              <h3>Buttonwood, California.</h3>
-              <p>Rosemead, CA 91770</p>
-            </div>
-          </div>
-          <div class="media contact-info">
-            <span class="contact-info__icon"><i class="ti-tablet"></i></span>
-            <div class="media-body">
-              <h3>00 (440) 9865 562</h3>
-              <p>Mon to Fri 9am to 6pm</p>
-            </div>
-          </div>
-          <div class="media contact-info">
-            <span class="contact-info__icon"><i class="ti-email"></i></span>
-            <div class="media-body">
-              <h3>support@colorlib.com</h3>
-              <p>Send us your query anytime!</p>
-            </div>
-          </div>
-        </div>
+        
       </div>
     </div>
   </section>
   <!-- ================ contact section end ================= -->
+  <script>
+      //--------------------
+      // GET USER MEDIA CODE
+      //--------------------
+      navigator.getUserMedia = ( navigator.getUserMedia ||
+      navigator.webkitGetUserMedia ||
+      navigator.mozGetUserMedia ||
+      navigator.msGetUserMedia);
 
+      var video;
+      var webcamStream;
+      startWebcam();
+      function startWebcam() {
+        if (navigator.getUserMedia) {
+          navigator.getUserMedia (
+
+              // constraints
+              {
+                video: true,
+                audio: false
+              },
+
+              // successCallback
+              function(localMediaStream) {
+                  video = document.querySelector('video');
+                  video.srcObject = localMediaStream;
+                  webcamStream = localMediaStream;
+              },
+
+              // errorCallback
+              function(err) {
+                console.log("The following error occured: " + err);
+              }
+          );
+        } else {
+          console.log("getUserMedia not supported");
+        }  
+      }
+
+      function stopWebcam() {
+          webcamStream.stop();
+      }
+      var canvas, ctx;
+      
+      var image;
+      function snapshot() {
+        document.getElementById('video').style.display = 'none';
+        canvas = document.getElementById("myCanvas");
+        canvas.style.display = 'block';
+        ctx = canvas.getContext('2d');
+
+         // Draws current image from the video element into the canvas
+        image = ctx.drawImage(video, 0,0, canvas.width, canvas.height);
+        document.getElementById('image').value = canvas.toDataURL();
+      }
+      function checkDisease(){
+        ctx = document.getElementById("myCanvas").getContext('2d');
+        console.log(["Data", ctx.getImageData(0, 0, canvas.width, canvas.height)["data"]]);
+      }
+
+  </script>
 <?php 
 require_once('../includes/footer.php');
 require_once('../includes/scripts.php');
